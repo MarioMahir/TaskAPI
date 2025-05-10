5204f427922aff74212c1c5fdd76302f8e9600d3
Mario Alejandro Sabala Encarnación <142932859+MarioMahir@users.noreply.github.com>
2025-05-09 22:19:58 -0400

    Implementación de la Etapa 2

---
+++ TaskAPI/Helpers/TaskDelegates.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+
+namespace TaskAPI.Helpers
+{
+    public static class TaskDelegates
+    {
+        public delegate bool ValidarTarea<T>(TaskAPI.Models.Task<T> tarea);
+
+        public static Func<DateTime, int> DiasRestantes = DueDate =>
+        {
+            return (DueDate - DateTime.UtcNow).Days;
+        };
+
+        public static Action<TaskAPI.Models.Task<string>> NotificarCreacion = t => 
+        {
+            Console.WriteLine($"[NOTIFICACIÓN] Se creó la tarea: {t.Description} - Fecha límite: {t.DueDate}");
+        };
+    }
+}
